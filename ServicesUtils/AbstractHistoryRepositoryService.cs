659534b90f654ebe4b398a4d33e41be8061994ec
Yan <yanshibainu@gmail.com>
2021-02-01 14:55:02 +0800

    HistoryRepository

---
+++ ServicesUtils/AbstractHistoryRepositoryService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace utils_lib.ServicesUtils
+{
+    public abstract class AbstractHistoryRepositoryService<TEntity, THistoryEntity>: IHistoryRepositoryService<TEntity, THistoryEntity>
+        where THistoryEntity : class, new() where TEntity : class, new()
+    {
+        protected readonly DbContext Context;
+
+        protected AbstractHistoryRepositoryService(DbContext context)
+        {
+            Context = context;
+        }
+
+        public THistoryEntity ToHistory(TEntity entity)
+        {
+            var newEntity = Context.Entry(new THistoryEntity());
+            newEntity.CurrentValues.SetValues(entity);
+            Context.Add(newEntity.Entity);
+            Context.SaveChanges();
+
+            return newEntity.Entity;
+        }
+
+        public TEntity HistoryBack(THistoryEntity entity)
+        {
+            var newEntity = Context.Entry(new TEntity());
+            newEntity.CurrentValues.SetValues(entity);
+            Context.Add(newEntity.Entity);
+            Context.SaveChanges();
+
+            return newEntity.Entity;
+        }
+    }
+}
---
+++ ServicesUtils/IHistoryRepositoryService.cs
@@ -0,0 +1,9 @@
+﻿namespace utils_lib.ServicesUtils
+{
+    public interface IHistoryRepositoryService<TEntity, THistoryEntity>
+    {
+        THistoryEntity ToHistory(TEntity entity);
+
+        TEntity HistoryBack(THistoryEntity entity);
+    }
+}
