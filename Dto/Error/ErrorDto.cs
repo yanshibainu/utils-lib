ff5e3436395c5efabc796d021d28646752ae604d
Yan <yanshibainu@gmail.com>
2021-02-09 21:33:47 +0800

    add FieldErrors

--- Dto/Error/ErrorDto.cs
+++ Dto/Error/ErrorDto.cs
@@ -1,7 +1,11 @@
-﻿namespace utils_lib.Dto.Error
+﻿using System.Collections.Generic;
+
+namespace utils_lib.Dto.Error
 {
     public class ErrorDto
     {
         public string Error { get; set; }
+
+        public IList<FieldErrorDto> FieldErrors { get; set; }
     }
 }
