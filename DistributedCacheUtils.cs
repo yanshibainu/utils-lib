373037f2ec6c114c02f8c715aa257dd29fbbb57e
Yan <yanshibainu@gmail.com>
2021-11-06 15:19:28 +0800

    add DistributedCacheUtils

---
+++ DistributedCacheUtils.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace utils_lib
+{
+    public static class DistributedCacheUtils
+    {
+        public static string GetKey(HttpContext httpContext)
+        {
+            return new StringBuilder()
+                .Append(httpContext.Request.Scheme)
+                .Append("://")
+                .Append(httpContext.Request.Host)
+                .Append(httpContext.Request.PathBase)
+                .Append(httpContext.Request.Path)
+                .Append(httpContext.Request.QueryString)
+                .ToString();
+        }
+
+        public static byte[] ObjectToByteArray(object obj)
+        {
+            var binaryFormatter = new BinaryFormatter();
+            using var memoryStream = new MemoryStream();
+            binaryFormatter.Serialize(memoryStream, obj);
+            return memoryStream.ToArray();
+        }
+
+        public static T ByteArrayToObject<T>(byte[] bytes)
+        {
+            using var memoryStream = new MemoryStream();
+            var binaryFormatter = new BinaryFormatter();
+            memoryStream.Write(bytes, 0, bytes.Length);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            var obj = binaryFormatter.Deserialize(memoryStream);
+            return (T)obj;
+        }
+    }
+}
---
+++ Filters/DistributedCacheResultFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace utils_lib.Filters
+{
+    public class DistributedCacheResultFilter : Attribute, IResultFilter
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public DistributedCacheResultFilter(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            if (context.HttpContext.Request.Method.ToLower() != "get")return;
+            if (!(context.Result is OkObjectResult result)) return;
+
+            var cacheKey = DistributedCacheUtils.GetKey(context.HttpContext);
+
+            if (_distributedCache.Get(cacheKey) == null)
+                _distributedCache.Set(cacheKey, DistributedCacheUtils.ObjectToByteArray(result.Value));
+        }
+    }
+}
